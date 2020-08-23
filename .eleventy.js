2a445847be7e3675328e7919a5e7523ea355b22f
Kris <kriskuiper74@gmail.com>
2020-08-23 21:19:54 +0200

    chore: add syntax highlight plugin for code blocks

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,19 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+
 const use404 = require('./utils/use-404')
 const minifyHtml = require('./utils/minify-html')
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addLayoutAlias('default', 'layouts/default.html')
+
   eleventyConfig.setBrowserSyncConfig(use404)
 
   eleventyConfig.addPassthroughCopy({ 'src/static/styles': 'styles' })
   eleventyConfig.addPassthroughCopy({ 'src/static/fonts': 'fonts' })
 
   eleventyConfig.addTransform('htmlmin', minifyHtml)
+
+  eleventyConfig.addPlugin(syntaxHighlight)
 
   return {
     dir: {
